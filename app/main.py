ad2c1c61a0bf68a08ba35da561b158016e4aa6e3
1078xd <alopex299@gmail.com>
2025-11-20 02:01:50 +0100

    update main code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
-import time
-from app.pipes.pipe_binance import run_pipe_binance
+def main():
+    import time
+    from app.pipes.pipe_binance import run_pipe_binance
 
-COIN_LIMIT = 1000
-DAYS_BACK = 3650
+    COIN_LIMIT = 1000
+    DAYS_BACK = 3650
 
-print("Starting Crypitibapitiboo")
+    print("Starting Crypitibapitiboo")
 
-start = time.perf_counter()
-run_pipe_binance(COIN_LIMIT, DAYS_BACK)
-end = time.perf_counter()
+    start = time.perf_counter()
+    run_pipe_binance(COIN_LIMIT, DAYS_BACK)
+    end = time.perf_counter()
 
-print(f"TOTAL TIME: {end - start:.2f} seconds")
-print("All pipes finished!")
+    print(f"TOTAL TIME: {end - start:.2f} seconds")
+    print("All pipes finished!")
